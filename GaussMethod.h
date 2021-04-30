b27cb766dc5eb838b708fe15915afa1bb5e68ee7
Roker2 <dimaminko2000@gmail.com>
2021-04-30 18:13:15 +0300

    GaussMethod: Add code
    From https://prog-cpp.ru/gauss/
    
    Test main.cpp:
    ===================================================
    
    using namespace std;
    
    int main()
    {
        cout << "Hello World!" << endl;
        SquareMatrix a(3, {
                           {2, 4, 1},
                           {5, 2, 1},
                           {2, 3, 4},
                       });
        Matrix y{1, 3, {{36, 47, 37}}};
        y.transpose();
        cout << GaussMethod::method(a, y) << std::endl;
        return 0;
    }
    ===================================================

--- GaussMethod.h
+++ GaussMethod.h
@@ -1,11 +1,16 @@
 #ifndef GAUSSMETHOD_H
 #define GAUSSMETHOD_H
 
+#include "Appxomate.h"
+#include "SquareMatrix.h"
 
 class GaussMethod
 {
 public:
     GaussMethod() = delete;
+    // a - уравнения в виде матрицы
+    // y - правая часть уравнения в виде вектора
+    static Matrix method(SquareMatrix a, Matrix y);
 };
 
 #endif // GAUSSMETHOD_H
