345f8d3d0eb592ffd3db2b968fdddbefc61553c9
Roker2 (Dmitry Minko) <dimaminko2000@gmail.com>
2021-05-13 13:15:05 +0300

    main: Add functions for different tasks

--- main.cpp
+++ main.cpp
@@ -8,23 +8,49 @@
 
 using namespace std;
 
+void testTask1()
+{
+    std::vector<FuncPtr> gfuncs{
+            Func::makeFunc("x1 ^ 2 + x3 ^ 2 - 2"),
+            Func::makeFunc("x3 - x2"),
+            Func::makeFunc("-x1 + 1"),
+        };
+    FuncCPtr fFunc = Func::makeFunc("x1 ^ 2 - 2 * x1 * x2 + x2 ^ 2 + x3 ^ 2");
+    Properties vectorx{{"x1", 1}, {"x2", 1}, {"x3", 1}};
+
+    Resolver::resolver(gfuncs, vectorx, fFunc);
+}
+
+void testTask2()
+{
+    std::vector<FuncPtr> gfuncs{
+            Func::makeFunc("(x1 - 2) ^ 2 + (x3 - 2) ^ 2 - 2"),
+            Func::makeFunc("-x2 + 1"),
+            Func::makeFunc("x1 + x2 + x3 - 10"),
+        };
+    FuncCPtr fFunc = Func::makeFunc("x1 ^ 2 + 2 * x1 * x2 + x2 ^ 2 + 2 * x3 ^ 2");
+    Properties vectorx{{"x1", 1}, {"x2", 1}, {"x3", 1}};
+
+    Resolver::resolver(gfuncs, vectorx, fFunc);
+}
+
+void dimaTask()
+{
+    std::vector<FuncPtr> gfuncs{
+        Func::makeFunc("(x1 + x2) ^ 2 + (x3 - 1) ^ 2 - 5"),
+        Func::makeFunc("-2 * x1 + 3 * x2 + x3 ^ 2 - 4"),
+        Func::makeFunc("-x1"),
+    };
+    FuncCPtr fFunc = Func::makeFunc("(x1 - 2 * x2 - x3) ^ 2 + 3 * x1 - 4 * x2 +x3");
+    Properties vectorx{{"x1", 0}, {"x2", 1}, {"x3", -1}};
+
+    Resolver::resolver(gfuncs, vectorx, fFunc);
+}
+
 int main()
 {
     //tests();
-    Properties vectorx{{"x1", 1}, {"x2", 1}, {"x3", 1}};
-    /*std::vector<FuncPtr> gfuncs{
-        Func("x1 ^ 2 + x3 ^ 2 - 2"),
-        Func("x3 - x2"),
-        Func("-x1 + 1"),
-    };
-    Func fFunc("x1 ^ 2 - 2 * x1 * x2 + x2 ^ 2 + x3 ^ 2");*/
-    std::vector<FuncPtr> gfuncs{
-        Func::makeFunc("(x1 - 2) ^ 2 + (x3 - 2) ^ 2 - 2"),
-        Func::makeFunc("-x2 + 1"),
-        Func::makeFunc("x1 + x2 + x3 - 10"),
-    };
-    FuncCPtr fFunc = Func::makeFunc("x1 ^ 2 + 2 * x1 * x2 + x2 ^ 2 + 2 * x3 ^ 2");
-
-    Resolver::resolver(gfuncs, vectorx, fFunc);
+    //dimaTask();
+    testTask2();
     return 0;
 }
