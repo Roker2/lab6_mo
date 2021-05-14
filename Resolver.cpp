0e14f1de609e4225b4829177005352fad76f9e8f
Dmitry <dimaminko2000@gmail.com>
2021-05-14 12:08:34 +0300

    Resolver: Fix calculating b and reorganize couts

--- Resolver.cpp
+++ Resolver.cpp
@@ -102,8 +102,7 @@ void Resolver::resolver(std::vector<FuncPtr>& gFuncs, const Properties& vectorx,
     for (int i = 0; i < fVector.getM(); i++)
         c[0][i] = fVector[i][0]; // минус, так как конвертируем из задачи минимизации в задачу максимизации
 
-    std::cout << fVector << std::endl;
-    std::cout << c << std::endl << std::endl;
+    std::cout << "\nc\n" << c << std::endl << std::endl;
 
     // заполняем матрицу элементами из gVectors
     Matrix As(n + vectorx.size(), n + 2 * vectorx.size());
@@ -116,21 +115,26 @@ void Resolver::resolver(std::vector<FuncPtr>& gFuncs, const Properties& vectorx,
     }
     for(size_t i = 0; i < 2 * vectorx.size(); i++)
         As[i][i + vectorx.size()] = 1;
-    std::cout << As << std::endl;
+    std::cout << "A for simplex method\n" << As << std::endl;
 
-    Matrix b (1, As.getM());
     Matrix d_low(1, vectorx.size());
     for (size_t i = 0; i < vectorx.size(); i++)
         // если не входит в J0, то ставим -1, в противном случае 0, который стоит по умолчанию
         if (J0.find(i) == J0.end())
             d_low[0][i] = -1;
 
-    Matrix temp = A * d_low;
-
-    std::cout << "d_low: " << d_low << std::endl;
+    std::cout << "d_low\n" << d_low << std::endl;
+    d_low.transpose();
+    Matrix b = Matrix(A) * d_low;
+    b.transpose();
+    d_low.transpose();
+    b.resize(1, b.getN() + d_low.getN());
+    for (int i = 0; i < d_low.getN(); i++)
+        b[0][i + vectorx.size()] = 1 - d_low[0][i]; // 1, так как d_up всегда состоит из единиц
+    std::cout << "b\n" << b << std::endl;
 
     std::set<int> Jb;
     for (int i = n; i < As.getN(); i++)
         Jb.insert(i);
-    std::cout << Jb << std::endl;
+    std::cout << "Jb: " << Jb << std::endl;
 }
